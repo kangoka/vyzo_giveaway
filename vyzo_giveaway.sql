57921a7905544d44bb45af6412c54f017f1868e8
vyzo <51883097+kangoka@users.noreply.github.com>
2022-10-05 18:06:40 +0700

    Added sql file

---
+++ vyzo_giveaway.sql
@@ -0,0 +1,48 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.4.24-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             11.0.0.5919
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Dumping database structure for kntl
+CREATE DATABASE IF NOT EXISTS `kntl` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `kntl`;
+
+-- Dumping structure for table kntl.vyzo_giveaway_code
+CREATE TABLE IF NOT EXISTS `vyzo_giveaway_code` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(50) NOT NULL,
+  `maxuse` tinyint(4) NOT NULL DEFAULT 0,
+  `reward` varchar(50) NOT NULL DEFAULT '0',
+  `quantity` int(11) DEFAULT 0,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `code` (`code`)
+) ENGINE=InnoDB AUTO_INCREMENT=68 DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table kntl.vyzo_giveaway_code: ~2 rows (approximately)
+/*!40000 ALTER TABLE `vyzo_giveaway_code` DISABLE KEYS */;
+/*!40000 ALTER TABLE `vyzo_giveaway_code` ENABLE KEYS */;
+
+-- Dumping structure for table kntl.vyzo_giveaway_log
+CREATE TABLE IF NOT EXISTS `vyzo_giveaway_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `identifier` varchar(50) NOT NULL,
+  `code` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table kntl.vyzo_giveaway_log: ~0 rows (approximately)
+/*!40000 ALTER TABLE `vyzo_giveaway_log` DISABLE KEYS */;
+/*!40000 ALTER TABLE `vyzo_giveaway_log` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
