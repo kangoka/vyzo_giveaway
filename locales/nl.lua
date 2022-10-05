591c883ddd2fa744f452d043ae0adbae8576a8a5
Raoul <50175388+HariboInc@users.noreply.github.com>
2022-10-05 22:00:15 +0200

    Create nl.lua

---
+++ locales/nl.lua
@@ -0,0 +1,18 @@
+Locales['nl'] = {
+    ['code_redeem']             = 'Code',
+    ['redeem']                  = 'Code verzilveren',
+    ['redeem_success']          = 'Code succesvol ontvangen',
+    ['empty']                   = 'Geen code ingevoerd',
+    ['format']                  = 'Code is ongeldig',
+    ['not_exist']               = 'Code bestaat niet',
+    ['limit']                   = 'Code is maximaal gebruikt',
+    ['full']                    = 'Je inventaris zit vol',
+    ['log_message']             = '`%s` heeft code `%s` gebruikt en kreeg `x%s %s`',
+    ['title']                   = 'Maak Giveaway',
+    ['code']                    = 'Code (laat leeg voor automatische selectie)',
+    ['maxUse']                  = 'Max Gebruik',
+    ['reward']                  = 'Prijs',
+    ['quantity']                = 'Aantal',
+    ['insert_success']          = 'Succesvol aan de database toegevoegd, kijk F8 om de code te bekijken',
+    ['insert_failed']           = 'Het is niet gelukt om deze aan de database toe te voegen.'
+}
