8ebf2cebeff839d973a60935a7bdfd97eee0a54d
vyzo <51883097+kangoka@users.noreply.github.com>
2022-10-13 20:59:51 +0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,17 +2,10 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 
-<<<<<<< HEAD
 name 'vyzo_giveaway'
 author 'vyzo'
 version '1.2.0'
 repository 'https://github.com/kangoka/vyzo_giveaway'
-=======
-name        'vyzo_giveaway'
-author      'vyzo'
-version     '1.1.1'
-repository  'soon'
->>>>>>> f551b2261c5241d3467dd0d54ab5cd7cf6444aa1
 description 'A script for admin to create a code for players to redeem'
 
 dependencies {
